e2a9e0f27c17340013c1511c4a84be54acce5f9c
Tomasz Giedziun <volhan@Tomis-MacBook-Pro.local>
2016-05-11 11:19:57 +0200

    sos

--- SSO/lab3/pierwsza_zad1.c
+++ SSO/lab3/pierwsza_zad1.c
@@ -1,8 +1,8 @@
-cd/**
+/**
 * Obliczanie ilosci liczb pierwszych z zadanego zakresu
 *
 * usage:
-* ./pierwsza_zad2 [zakres_start] [zakres_end]
+* ./pierwsza_zad1 [zakres_start] [zakres_end]
 */
 #include <stdlib.h>
 #include <errno.h>
@@ -10,10 +10,6 @@ cd/**
 #include <unistd.h>
 #include <sys/stat.h>
 
-#include <time.h>
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
 struct result_struct {
     int pocz;
     int koniec;
@@ -33,30 +29,27 @@ int pierwsza(int n)
 int main(int argc, char* argv[]) {
     int i, fd, res,    
     start=atoi(argv[1]),
-    end=atoi(argv[2]+1),
+    end=atoi(argv[2]),
     pierwszych=0;
 
     for(i = start; i<=end; i++) {
         if(pierwsza(i)==1)
             pierwszych++;
     }
-    
     printf("in: %d ,out: %d  = %d\n",start,end,pierwszych );
     // set data
     result.pocz = start;
     result.koniec = end;
     result.ile = pierwszych;
 
-    fd = open("results.txt", O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);//read and write premison owner S_IRUSR S_IWUSR
+    fd = open("fifoYOLO",O_RDWR);
 
     if(fd < 0 ) {
         perror("fd open error");
         exit(0);
     }
 
-    lockf(fd,F_LOCK,0);
-    write(fd,&result,sizeof(result));
-    lockf(fd,F_ULOCK,0);
+    res = write(fd,&result,sizeof(result));
     close(fd);
 
     return 0;
